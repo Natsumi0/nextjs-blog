9af0d62304c2adfcd099d83a4a4b37a48867863b
Natsumi Yoritate <natsumiy@NatsuminoMacBook-Air.local>
2021-02-07 02:35:33 +0900

    add Twitter Link

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm Natsu. I'm a web developer and a translator (English/Japanese). You can contact me on Twitter.</p>
+        <p>Hello, I'm Natsu. I'm a web developer and a translator (English/Japanese). You can contact me on <a href="https://twitter.com/natsu_JPN0" title="Twitter">Twitter</a>.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
